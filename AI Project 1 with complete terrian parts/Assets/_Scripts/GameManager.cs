947b41b8aeaa683db6bd71367341387ec1042fc3
OllytingXin <42716913+OllytingXin@users.noreply.github.com>
2018-09-15 13:46:42 -0400

    update

---
+++ AI Project 1 with complete terrian parts/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum gameStates {playing, death};
+
+public class GameManager : MonoBehaviour {
+
+    // Use this for initialization
+
+    public static GameManager GM;
+
+    public gameStates gameState;
+
+    public GameObject playerIns;
+
+    public GameObject mainCanvas;
+
+    public GameObject introCanvas;
+
+    public GameObject referCanvas;
+
+    public GameObject deathCanvas;
+
+	void Start () {
+        GM = this;
+        gameState = gameStates.playing;
+
+        if (mainCanvas) {
+            mainCanvas.SetActive(true);
+        }
+
+        if (introCanvas) {
+            introCanvas.SetActive(false);
+        }
+
+        if (referCanvas) {
+            referCanvas.SetActive(false);
+        }
+
+        if (deathCanvas) {
+            deathCanvas.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+### 09/15/2018
+The complete terrian parts have been put in one unity project!!!
