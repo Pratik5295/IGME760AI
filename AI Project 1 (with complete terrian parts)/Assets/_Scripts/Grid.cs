15d2d0d22eb4e92c9890f60ad31b6ed6bbd15ca5
Pratik Prasad Shringarpure (RIT Student) <pps5677@ad.rit.edu>
2018-09-27 19:23:12 -0400

    New Scene A*

---
+++ AI Project 1 (with complete terrian parts)/Assets/_Scripts/Grid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    // Use this for initialization
+
+    private Node[,] graph;
+    private float GridPosX;
+    private float GridPosY;
+
+    private float CellSize = 1f;
+    private float GridSizeX = 30f;
+    private float GridSizeY = 30f;
+
+    private float numberofCellsX;
+    private float numberofCellsY;
+
+
+
+    public Vector3 startOfGrid;
+
+
+	void Start () {
+
+        startOfGrid = this.transform.position - (transform.right * GridSizeX/2 + transform.forward * GridSizeY/2); // gets the bottom left starting point of the grid
+
+        numberofCellsX = GridSizeX / CellSize;
+        numberofCellsY = GridSizeY / CellSize;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(GridSizeX, 1, GridSizeY));
+    }
+}
---
+++ AI Project 1 (with complete terrian parts)/Assets/_Scripts/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node  {
+
+
+    public Vector3 PositionInWorld;
+    public bool IsWalkable;
+    
+
+    public Node(bool status, Vector3 pos)
+    {
+        IsWalkable = status;
+        PositionInWorld = pos;
+    }
+}
