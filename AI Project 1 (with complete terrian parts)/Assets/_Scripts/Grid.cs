9281dc21be807d2cd959eb0dac5a6ff426331d52
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2018-09-29 11:54:23 -0400

    Grid A*

--- AI Project 1 (with complete terrian parts)/Assets/_Scripts/Grid.cs
+++ AI Project 1 (with complete terrian parts)/Assets/_Scripts/Grid.cs
@@ -10,35 +10,123 @@ public class Grid : MonoBehaviour {
     private float GridPosX;
     private float GridPosY;
 
-    private float CellSize = 1f;
-    private float GridSizeX = 30f;
-    private float GridSizeY = 30f;
+    public Terrain terrain;
 
-    private float numberofCellsX;
-    private float numberofCellsY;
+    private float terrainHeight;
+    private float terrainWidth;
+
+   
+
+    private int CellSize = 1;
+    private int GridSizeX = 75;     //width of the grid
+    private int GridSizeY = 75;     //height of the grid
 
 
+    public int numberofCellsX;
+    private int numberofCellsY;
+
+    private LayerMask obstacles = 512;  //integer value for layer 9 //// 1<<9 would also work
 
     public Vector3 startOfGrid;
+    private Vector3 gridpoint;
 
+  
+    //to check raycasts hit some collider
+    private bool HitsAir;
+
+    
 
 	void Start () {
 
-        startOfGrid = this.transform.position - (transform.right * GridSizeX/2 + transform.forward * GridSizeY/2); // gets the bottom left starting point of the grid
-
-        numberofCellsX = GridSizeX / CellSize;
+        
+        
+        numberofCellsX =GridSizeX/ CellSize;
         numberofCellsY = GridSizeY / CellSize;
 
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+
+
+
+    }
+
+    private void Update()
+    {
+        GridDrawing();
+    }
+
+    void GridDrawing()
+    {
+        graph = new Node[GridSizeX ,GridSizeY];
+
+        startOfGrid = this.transform.position - transform.right *GridSizeX / 2 - transform.forward * GridSizeY / 2; // gets the bottom left starting point of the grid
+
+        for (int x = 0; x < numberofCellsX; x++)
+        {
+
+            for(int y = 0; y< numberofCellsY; y++)
+            {
+                gridpoint = startOfGrid + Vector3.right * (x *CellSize + 0.5f) + Vector3.forward * (y*CellSize + 0.5f);
+                bool IsWalkable = !(Physics.CheckSphere(gridpoint, CellSize, obstacles));
+
+
+                //raycasting code
+                Ray checkray = new Ray(gridpoint + transform.up * 200, (-transform.up));
+                RaycastHit rayhit;
+                if(Physics.Raycast(checkray,out rayhit, 250))
+                {
+                    if(rayhit.collider.gameObject.tag == "Obstacle")
+                    {
+                        IsWalkable = false;
+                    }
+                  
+
+                    if(rayhit.collider.gameObject.tag == "Ground")
+                    {
+                        IsWalkable = true;
+                    }
+
+                    gridpoint.y = rayhit.point.y;
+                }
+                else
+                {
+                    HitsAir = true;
+                }
+
+
+
+                graph[x, y] = new Node(IsWalkable, gridpoint);
+
+
+
+
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(this.transform.position, new Vector3(GridSizeX, 1, GridSizeY));
+
+        if(graph != null)
+        {
+            foreach(Node n in graph)
+            {
+                if (n.IsWalkable)
+                {
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawCube(n.PositionInWorld, Vector3.one * 0.8f);
+                }
+
+                else if(!n.IsWalkable)
+                {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.PositionInWorld, Vector3.one * 0.8f);
+                }
+
+
+            }
+        }
     }
 }
--- AI Project 1 (with complete terrian parts)/Assets/_Scripts/Node.cs
+++ AI Project 1 (with complete terrian parts)/Assets/_Scripts/Node.cs
@@ -7,11 +7,14 @@ public class Node  {
 
     public Vector3 PositionInWorld;
     public bool IsWalkable;
+
+  
     
 
     public Node(bool status, Vector3 pos)
     {
         IsWalkable = status;
         PositionInWorld = pos;
+       
     }
 }
